7351b2dcd1158be455cd4c350c56b319970ee764
mowoe <git@mowoe.com>
2024-09-13 19:50:38 +0200

    fix lambda function name reference

--- tofu/main.tf
+++ tofu/main.tf
@@ -106,6 +106,6 @@ resource "aws_lambda_function" "withings_garmin_sync_function" {
 resource "aws_lambda_permission" "allow_s3" {
   statement_id  = "AllowExecutionFromS3"
   action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.my_lambda.function_name
+  function_name = aws_lambda_function.withings_garmin_sync_function.function_name
   principal     = "s3.amazonaws.com"
 }
